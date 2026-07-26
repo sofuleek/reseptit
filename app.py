c738f9712ea8a00e61354cb42e90b7b19607b308
Sofia Shashkina <sofiashashkina@Sofia-MacBook-Pro-3.local>
2026-07-26 11:17:22 +0300

    Add new item form

--- app.py
+++ app.py
@@ -12,6 +12,23 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/new_item")
+def new_item():
+    return render_template("new_item.html")
+
+@app.route("/create_item", methods=["POST"])
+def create_item():
+    title = request.form["title"]
+    description = request.form["description"]
+    preparation_time = request.form["preparation_time"]
+    user_id = session["user_id"]
+
+    sql = """INSERT INTO items (title, description, preparation_time, user_id)
+            VALUES (?, ?, ?, ?)"""
+    db.execute(sql, [title, description, preparation_time, user_id])
+
+    return redirect("/")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -42,10 +59,13 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
 
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
@@ -53,5 +73,6 @@ def login():
 
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE items (
+    id INTEGER PRIMARY KEY,
+    title TEXT UNIQUE,
+    description TEXT,
+    preparation_time INTEGER,
+    user_id INTEGER REFERENCES users
+);
