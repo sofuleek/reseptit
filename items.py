e5f784eb86684b77e1440b03ca6d11d91aed00ff
Sofia Shashkina <sofiashashkina@Sofia-MacBook-Pro-3.local>
2026-08-02 17:09:50 +0300

    Add remove item feature

--- items.py
+++ items.py
@@ -27,3 +27,7 @@ def update_item(item_id, title, description, preparation_time):
                               preparation_time = ?
                           WHERE id = ?"""
     db.execute(sql, [title, description, preparation_time, item_id])
+
+def remove_item(item_id):
+    sql = "DELETE FROM items WHERE id = ?"
+    db.execute(sql, [item_id])
