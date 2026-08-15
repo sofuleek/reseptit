402149f72a12712244852d28eb14442f19e02a54
Sofia Shashkina <sofiashashkina@Sofia-MacBook-Pro-3.local>
2026-08-15 21:56:00 +0300

    Check invalid form inputs

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 import config
 import db
 import recipes
+import re
 
 app = Flask(__name__)
 app.secret_key = config.secret_key
@@ -45,8 +46,14 @@ def create_recipe():
     require_login()
 
     title = request.form["title"]
+    if not title or len(title) > 50:
+        abort(403)
     description = request.form["description"]
+    if not description or len(description) > 1000:
+        abort(403)
     preparation_time = request.form["preparation_time"]
+    if not re.search("^[1-9][0-9]{0,3}$", preparation_time):
+        abort(403)
     user_id = session["user_id"]
 
     recipes.add_recipe(title, description, preparation_time, user_id)
